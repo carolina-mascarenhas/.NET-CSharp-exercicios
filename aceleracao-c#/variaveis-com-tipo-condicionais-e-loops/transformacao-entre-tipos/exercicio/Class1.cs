f71aa001a6a7567649948ffbb898602441743842
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-08-28 20:15:39 -0300

    feat: exercicios do dia

---
+++ aceleracao-c#/variaveis-com-tipo-condicionais-e-loops/transformacao-entre-tipos/exercicio/Class1.cs
@@ -0,0 +1,32 @@
+﻿// namespace variable;
+// public class Class1
+// {
+//   public string someString = "oi";
+// }
+
+// namespace variable;
+// public class Class1
+// {
+//   public int intEntry = 42;
+//   public double doubleResponse;
+
+//   public void Transform()
+//   {
+//     doubleResponse = (double) intEntry;
+//   }
+// }
+
+namespace variable;
+public class Class1
+{
+  public string strEntry = "42";
+  public int intResponse;
+
+  public void DoConversion()
+  {
+    if(strEntry is string)
+    {
+      intResponse = Convert.ToInt32(strEntry);
+    }
+  }
+}
