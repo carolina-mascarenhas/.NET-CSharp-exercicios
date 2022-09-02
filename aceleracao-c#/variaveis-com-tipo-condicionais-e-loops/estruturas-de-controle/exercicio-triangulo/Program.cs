97204bc855526fa470efd28adc729e1878e0d0cf
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-09-02 17:25:37 -0300

    feat: exercicio triangulo

---
+++ aceleracao-c#/variaveis-com-tipo-condicionais-e-loops/estruturas-de-controle/exercicio-triangulo/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace exercicio_triangulo;
+
+class Program
+{
+  static void Main()
+  {
+    Triangle.IdentifyTriangle(2, 2, 3);
+  }
+}
---
+++ aceleracao-c#/variaveis-com-tipo-condicionais-e-loops/estruturas-de-controle/exercicio-triangulo/Triangle.cs
@@ -0,0 +1,24 @@
+﻿namespace exercicio_triangulo;
+
+public class Triangle
+{
+  public static string IdentifyTriangle(double xSide, double ySide, double zSide)
+  {       
+    var name = "";
+
+    if(xSide == ySide && xSide == zSide)
+    {
+      name = "Triângulo Equilátero";
+    }
+    else if((xSide == ySide) || (xSide == zSide) || (ySide ==zSide))
+    {
+      name = "Triângulo Isóscele";
+    }
+    else
+    {
+      name = "Triângulo Escaleno";
+    }
+
+    return name;
+  }
+}
