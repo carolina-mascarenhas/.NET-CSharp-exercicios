f8b7c59930346214222b3014a63788bf2802af1b
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-09-02 16:54:58 -0300

    feat: exemplo switch case

---
+++ aceleracao-c#/variaveis-com-tipo-condicionais-e-loops/estruturas-de-controle/switch-case/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+  public static int number;
+  public static void Main()
+  {
+    switch (number)
+    {                
+      case > 0:
+        Console.WriteLine("maior que 0");
+        break;               
+      case 0:
+        Console.WriteLine("igual a zero");
+        break;
+      default:                    
+        Console.WriteLine("menor que zero");
+        break;
+    }
+  }            
+}
