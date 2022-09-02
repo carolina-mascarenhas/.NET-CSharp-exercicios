cbb1e27cf243236ac02fbc801669cf80df64b0ee
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-09-02 17:56:06 -0300

    feat: adicionando console no teste de triangulo

--- aceleracao-c#/variaveis-com-tipo-condicionais-e-loops/L7-estruturas-de-controle/exercicio-triangulo.Test/UnitTest1.cs
+++ aceleracao-c#/variaveis-com-tipo-condicionais-e-loops/L7-estruturas-de-controle/exercicio-triangulo.Test/UnitTest1.cs
@@ -7,9 +7,9 @@ public class UnitTest1
 {
     [Theory(DisplayName = "Deve classificar um triangulo")]
     [InlineData(2,2,2, "Triângulo Equilátero")]
-    [InlineData(5,6,6, "Triângulo Isóscele")]
-    [InlineData(6,2,6, "Triângulo Isóscele")]
-    [InlineData(6,6,2, "Triângulo Isóscele")]
+    [InlineData(5,6,6, "Triângulo Isósceles")]
+    [InlineData(6,2,6, "Triângulo Isósceles")]
+    [InlineData(6,6,2, "Triângulo Isósceles")]
     [InlineData(1,2,3, "Triângulo Escaleno")]
     public void TestIdentifyTriangle(double xSide, double ySide, double zSide, string name)
     {
@@ -17,4 +17,4 @@ public class UnitTest1
 
         resultName.Should().Be(name);
     }
-}   
+}
